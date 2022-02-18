98adb99943ea396751b7f27ae8f8d0b40cb97c89
sy4ph <nopointinn@gmail.com>
2022-02-18 16:02:34 +0300

    autonomous update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousParkingB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousParkingB.java
@@ -1,56 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
-/* Copyright (c) 2019 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import java.util.List;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
-@Autonomous(name = "AutoTest1", group = "Autonomous")
-public class AutonomousTest extends LinearOpMode {
+import java.util.List;
+@Autonomous(name = "ParkingBlue",group = "Autonomous")
+public class AutonomousParkingB extends LinearOpMode {
 
     private void initVuforia() {
         /*
@@ -133,14 +98,10 @@ public class AutonomousTest extends LinearOpMode {
     }
 
     public  void motorsSet(int BLplus, int BRplus, int FLplus,int FRplus) {
-        BRpos = BRpos + BRplus;
-        motorBackRight.setTargetPosition(BRpos);
-        BLpos = BLpos + BLplus;
-        motorBackLeft.setTargetPosition(BLpos);
-        FLpos = FLpos + FLplus;
-        motorFrontLeft.setTargetPosition(FLpos);
-        FRpos = FRpos + FRplus;
-        motorFrontRight.setTargetPosition(FRpos);
+        motorBackRight.setTargetPosition(BRpos + BRplus);
+        motorBackLeft.setTargetPosition(BLpos + BLplus);
+        motorFrontLeft.setTargetPosition(FLpos + FLplus);
+        motorFrontRight.setTargetPosition(FRpos + FRplus);
     }
     @Override
     public void runOpMode() {
@@ -236,45 +197,33 @@ public class AutonomousTest extends LinearOpMode {
             telemetry.addData("duckPosition", duckPosition);
         }
 // movement starts here
-        if (duckPosition == 1) {
-            //BL/BR/FL/FR
-            motorsSet(2800,2800,2800,2800);
-            sleep(1500);
 
+        //BL/BR/FL/FR
+        motorsSet(-7200,7200,7200,-7200);
+        waitTillOver();
+        motorsSet(1400,1400,1400,1400);
 
-            motorsSet(1300,0,1300,0);
-            sleep(1500);
-            motorHand.setPower(0.25);
-            motorHand.setTargetPosition(285);
-            sleep(650);
-            servoVal.setPower(-1.);
-            sleep(1500);
-            servoVal.setPower(0.);
-
-            motorsSet(-2800,2800,-2800,2800);
-            sleep(1500);
-            motorsSet(4800,4800,4800,4800);
-        }
-        if (duckPosition == 2) {
-            motorsSet(2800,2800,2800,2800);
-            sleep(1500);
-
-
-            motorsSet(1300,0,1300,0);
-            sleep(1500);
-            motorHand.setPower(0.25);
-            motorHand.setTargetPosition(155);
-            sleep(650);
-            servoVal.setPower(-1.);
-            sleep(1500);
-            servoVal.setPower(0.);
-
-            motorsSet(-2800,2800,-2800,2800);
-            sleep(1500);
-            motorsSet(4800,4800,4800,4800);
-        }
+//        if (duckPosition == 2) {
+//            motorsSet(2800,2800,2800,2800);
+//            sleep(1500);
+//
+//
+//            motorsSet(1300,0,1300,0);
+//            sleep(1500);
+//            motorHand.setPower(0.25);
+//            motorHand.setTargetPosition(155);
+//            sleep(650);
+//            servoVal.setPower(-1.);
+//            sleep(1500);
+//            servoVal.setPower(0.);
+//
+//            motorsSet(-2800,2800,-2800,2800);
+//            sleep(1500);
+//            motorsSet(4800,4800,4800,4800);
+//        }
         while (opModeIsActive()) {
             telemetry.addData("runtime",runtime.toString());
         }
     }
 }
+
