f63beec7c9e3ed94fd8ac3cd1f421c30f5107c43
sy4ph <nopointinn@gmail.com>
2022-03-01 09:26:49 +0300

    added standart config class; rewritten Auto and TeleOp classes to match new configuration;

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoNoTFODBlue.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//todo change values, they are not final
+
+@Autonomous(name = "Autonomous Parking + LVL3 / Blue", group = "Autonomous")
+public class AutoNoTFODBlue extends LinearOpMode {
+
+    StandartConfig robot = new StandartConfig();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public int FLpos = 0;
+    public int FRpos = 0;
+    public int BLpos = 0;
+    public int BRpos = 0;
+
+    public  void motorsSet(int BLplus, int BRplus, int FLplus,int FRplus) {
+        BRpos = BRpos + BRplus;
+        robot.motorBackRight.setTargetPosition(BRpos);
+        BLpos = BLpos + BLplus;
+        robot.motorBackLeft.setTargetPosition(BLpos);
+        FLpos = FLpos + FLplus;
+        robot.motorFrontLeft.setTargetPosition(FLpos);
+        FRpos = FRpos + FRplus;
+        robot.motorFrontRight.setTargetPosition(FRpos);
+    }
+    @Override
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+
+        robot.motorBackLeft.setPower(1.);
+        robot.motorBackRight.setPower(1.);
+        robot.motorFrontRight.setPower(1.);
+        robot.motorFrontLeft.setPower(1.);
+
+        waitForStart();
+        runtime.reset();
+
+        motorsSet(2800,2800,2800,2800);
+        sleep(1500);
+
+        motorsSet(800,-800,800,-800);
+        sleep(1500);
+
+        robot.motorHand.setPower(0.25);
+        robot.motorHand.setTargetPosition(285);
+        sleep(650);
+
+        robot.servoVal.setPower(-0.3);
+        sleep(1500);
+
+        robot.servoVal.setPower(0.);
+
+        motorsSet(-2400,2400,-2400,2400);
+        robot.motorHand.setTargetPosition(100);
+        sleep(1500);
+
+        motorsSet(7800,7800,7800,7800);
+
+        while (opModeIsActive()) {
+            telemetry.addData("runtime",runtime.toString());
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTC.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode;
+
+import static java.lang.Thread.sleep;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="MecTestV5", group="Linear Opmode")
+public class TeleOpTC extends OpMode {
+
+    // Init standart configuration from StandartConfig class
+    StandartConfig robot = new StandartConfig();
+    private ElapsedTime runtime = new ElapsedTime();
+    private boolean changesMade = false;
+
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+    public void start() {
+        runtime.reset();
+    }
+
+    public void loop() {
+        if (gamepad1.right_trigger != 0) {
+            robot.servoVal.setPower(gamepad1.right_trigger);
+        }
+        if (gamepad1.left_trigger != 0) {
+            robot.servoVal.setPower(-gamepad1.left_trigger);
+        }
+        if ((gamepad1.right_trigger == 0)&(gamepad1.left_trigger == 0)) {
+            robot.servoVal.setPower(0.);
+        }
+        if (gamepad1.a) {
+            robot.motorHand.setTargetPosition(0);
+            robot.motorHand.setPower(0.1);
+        }
+        if (gamepad1.b) {
+            robot.motorHand.setTargetPosition(155); //-20
+            robot.motorHand.setPower(0.15);
+        }
+        if (gamepad1.y) {
+            robot.motorHand.setTargetPosition(300); //-15
+            robot.motorHand.setPower(0.11);
+        }
+        if (gamepad1.right_bumper) {
+            robot.servoVal.setPower(0.);
+        }
+        if ((gamepad1.dpad_up) & (changesMade)) {
+            robot.motorHand.setTargetPosition(robot.motorHand.getTargetPosition() + 25);
+            robot.motorHand.setPower(0.5);
+            changesMade = false;
+        }
+        if ((gamepad1.dpad_down) & (changesMade)) {
+            robot.motorHand.setTargetPosition(robot.motorHand.getTargetPosition() - 25);
+            robot.motorHand.setPower(0.5);
+            changesMade = false;
+        }
+        if ((!gamepad1.dpad_down) & (!gamepad1.dpad_up)) {
+            changesMade = true;
+        }
+        if ((gamepad1.left_bumper) & (gamepad1.a)) {
+            robot.motorHand.setPower(0.);
+            try {
+                sleep(900);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            robot.motorHand.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.motorHand.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        double max;
+/**
+ * I have no idea how this stuff works. It just does.
+ */
+//todo 28/02/22 please rewrite this
+        // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+        double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+        double lateral = gamepad1.left_stick_x;
+        double yaw = gamepad1.right_stick_x;
+
+        // Combine the joystick requests for each axis-motion to determine each wheel's power.
+        // Set up a variable for each drive wheel to save the power level for telemetry.
+        double leftFrontPower = axial + lateral + yaw;
+        double rightFrontPower = axial - lateral - yaw;
+        double leftBackPower = axial - lateral + yaw;
+        double rightBackPower = axial + lateral - yaw;
+
+        // Normalize the values so no wheel power exceeds 100%
+        // This ensures that the robot maintains the desired motion.
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+
+        robot.motorFrontLeft.setPower(leftFrontPower);
+        robot.motorFrontRight.setPower(rightFrontPower);
+        robot.motorBackLeft.setPower(leftBackPower);
+        robot.motorBackRight.setPower(rightBackPower);
+
+        // Telemetry of hand DCMotor and runtime.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("TargetPos", robot.motorHand.getTargetPosition());
+        telemetry.addData("CurrentPos", robot.motorHand.getCurrentPosition());
+        telemetry.update();
+    }
+    public void stop() {
+        // Move hand to 0 position
+        robot.motorHand.setPower(0.1);
+        robot.motorHand.setTargetPosition(0);
+    }
+}
